44b5b4c4f2a06c91257f6cf54d81178a2f80e0f9
Tewodros Assefa <teddyassef23@gmail.com>
2023-12-08 05:08:54 -0600

    Zip code to city name convertor is done

---
+++ asset/scrip.js
@@ -0,0 +1,32 @@
+
+var zip = document.getElementById("frmSubmit");
+var lat, lon;
+var weatherApiURL;
+var requestUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat';
+var apikey = '9d21cee1b28b1481203a53b4d7fd6a12';
+var apikey1 = 'e2dbdc051fc56d2a9def7b404457bd7f';
+
+var tempDay1, tempDay2,tempDay3,tempDay4,tempDay5,tempDay6;
+var minTempDay1, minTempDay2,mimTempDay3,minTempDay4,minTempDay5,mimTempDay6;
+var maxTempDay1, maxTempDay2,maxTempDay3,maxTempDay4,maaxTempDay5,maxTempDay6;
+var tempicoDay1, tempicoDay2,tempicoDay3,tempicoDay4,tempicoDay5,tempIcoDay6;
+
+
+
+
+var fullWeatherData
+function search(){
+  var  zip = document.getElementById("searhc").value;
+    
+    var cuntyCodeAPI = 'http://api.openweathermap.org/geo/1.0/zip?zip=' + zip + '&appid=9d21cee1b28b1481203a53b4d7fd6a12'
+    // var cuntyCodeAPI = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=9d21cee1b28b1481203a53b4d7fd6a12'
+
+fetch(cuntyCodeAPI)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  }); 
+
+}
